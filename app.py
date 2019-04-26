b4575ac5e5bd5636de3f7147adfe9dc4f7eb627b
dev-fahim <fahim6668@gmail.com>
2019-04-26 09:10:00 +0600

    c

--- app.py
+++ app.py
@@ -54,14 +54,72 @@ def webhook():
                     entity, value = wit_response(messaging_text)
 
                     if entity == 'greetings':
-                        response = [
-                            {
-                                "type":"web_url",
-                                "url":"https://www.messenger.com",
-                                "title":"Visit Messenger"
+                        response = {
+                            "attachment": {
+                                "type": "template",
+                                "payload": {
+                                    "template_type": "list",
+                                    "top_element_style": "compact",
+                                    "elements": [
+                                        {
+                                            "title": "Classic T-Shirt Collection",
+                                            "subtitle": "See all our colors",
+                                            "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                                            "buttons": [
+                                                {
+                                                    "title": "View",
+                                                    "type": "web_url",
+                                                    "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                                                    "messenger_extensions": "true",
+                                                    "webview_height_ratio": "tall",
+                                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            "title": "Classic White T-Shirt",
+                                            "subtitle": "See all our colors",
+                                            "default_action": {
+                                                "type": "web_url",
+                                                "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                                                "messenger_extensions": "false",
+                                                "webview_height_ratio": "tall"
+                                            }
+                                        },
+                                        {
+                                            "title": "Classic Blue T-Shirt",
+                                            "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                                            "subtitle": "100% Cotton, 200% Comfortable",
+                                            "default_action": {
+                                                "type": "web_url",
+                                                "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                                                "messenger_extensions": "true",
+                                                "webview_height_ratio": "tall",
+                                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                            },
+                                            "buttons": [
+                                                {
+                                                    "title": "Shop Now",
+                                                    "type": "web_url",
+                                                    "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                                                    "messenger_extensions": "true",
+                                                    "webview_height_ratio": "tall",
+                                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                                }
+                                            ]
+                                        }
+                                    ],
+                                    "buttons": [
+                                        {
+                                            "title": "View More",
+                                            "type": "postback",
+                                            "payload": "payload"
+                                        }
+                                    ]
+                                }
                             }
-                        ]
-                        bot.send_button_message(sender_id, "Hello", response)
+                        }
+                        bot.send_message(sender_id, response)
 
                     elif entity == 'Ad_sense_income':
                         response = " Do you want to know how to start income using Adsense?"
